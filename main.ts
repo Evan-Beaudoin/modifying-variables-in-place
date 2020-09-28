946e0f803df794f73057ee0cb70e81be3ac316bc
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-09-28 14:04:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let Hungryness = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        Hungryness += 1
+        basic.showString("" + (Hungryness))
+    } else if (input.buttonIsPressed(Button.B)) {
+        Hungryness += -1
+        basic.showString("" + (Hungryness))
+    }
 })
